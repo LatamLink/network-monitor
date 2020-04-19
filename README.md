720372e46c4d86a46de085886e81dafb7380b2a2
Xavier Fernandez <xavier@crservers.com>
2020-04-18 22:47:29 -0600

    Create LatamLink Network Monitor

---
+++ README.md
@@ -0,0 +1,7 @@
+# LatamLink Infrastructure Monitor
+
+We strive to be transparent and focused on providing the most reliable API service as possible. Which is why we have published our real-time API Node Monitor. https://monitor.latamlink.io
+
+*Code forked from CryptoLions.io*
+ https://github.com/CryptoLions/EOS-Testnet-monitor
+Thanks!
---
+++ config.js
@@ -0,0 +1,31 @@
+var _reqInterval = 1000;
+
+var blockProducerList = [
+ {
+    "bp_name": "EOS Argentina",
+    "organisation": "EOS Argentina",
+    "location": "Buenos Aires, Argentina",
+    "node_addr": "boot.testnet.latamlink.io",
+    "port_http": "8888",
+    "port_ssl": "443",
+    "port_p2p": "9876"
+  },
+  {
+    "bp_name": "EOS Costa Rica",
+    "organisation": "EOS Costa Rica",
+    "location": "San Jose, Costa Rica",
+    "node_addr": "latamlink.eosio.cr",
+    "port_http": "443",
+    "port_ssl": "443",
+    "port_p2p": "9876"
+  },
+  {
+    "bp_name": "EOS Venezuela",
+    "organisation": "EOS Venezuela",
+    "location": "Caracas, Venezuela",
+    "node_addr": "latamlink.eosvenezuela.io",
+    "port_http": "80",
+    "port_ssl": "443",
+    "port_p2p": "9876"
+  },
+]
