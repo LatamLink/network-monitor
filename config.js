049441f5ce64e22736f5ee241e3699843b1860b0
Xavier Fernandez <xavier@crservers.com>
2020-06-19 11:10:18 -0600

    add test producers

--- config.js
+++ config.js
@@ -1,44 +1,194 @@
 var _reqInterval = 1000;
 
 var blockProducerList = [
- {
-   "bp_name": "eosio",
-   "organisation": "Latam Link",
-   "location": "Buenos Aires, Argentina",
-   "node_addr": "latamlink.eosio.cr",
-   "port_http": "443",
-   "port_ssl": "443",
-   "p2p_addr": "boot.testnet.latamlink.io",
-   "port_p2p": "9876"
-  },
-  {
-   "bp_name": "ar1",
-   "organisation": "EOS Argentina",
-   "location": "Buenos Aires, Argentina",
-   "node_addr": "api.testnet.lacchain.eosargentina.io",
-   "port_http": "443",
-   "port_ssl": "443",
-   "p2p_addr": "p2p.testnet.lacchain.eosargentina.io",
-   "port_p2p": "9870"
-   },
-   {
-    "bp_name": "cr1",
-    "organisation": "EOS Costa Rica",
-    "location": "San Jose, Costa Rica",
-    "node_addr": "latamlink.eosio.cr",
-    "port_http": "443",
-    "port_ssl": "443",
-    "p2p_addr": "latamlink.eosio.cr",
-    "port_p2p": "9876"
-  },
-  {
-    "bp_name": "ve1",
-    "organisation": "EOS Venezuela",
-    "location": "Caracas, Venezuela",
-    "node_addr": "lacchain.eosvenezuela.io",
-    "port_http": "443",
-    "port_ssl": "443",
-    "p2p_addr": "lacchain.eosvenezuela.io",
-    "port_p2p": "9876"
-  }
+{
+ "bp_name": "eosio",
+ "organisation": "Latam Link",
+ "location": "Buenos Aires, Argentina",
+ "node_addr": "latamlink.eosio.cr",
+ "port_http": "443",
+ "port_ssl": "443",
+ "p2p_addr": "boot.testnet.latamlink.io",
+ "port_p2p": "9876"
+},
+{
+ "bp_name": "ar1",
+ "organisation": "EOS Argentina",
+ "location": "Buenos Aires, Argentina",
+ "node_addr": "api.testnet.lacchain.eosargentina.io",
+ "port_http": "443",
+ "port_ssl": "443",
+ "p2p_addr": "p2p.testnet.lacchain.eosargentina.io",
+ "port_p2p": "9870"
+},
+{
+  "bp_name": "ar2",
+  "organisation": "EOS Argentina",
+  "location": "Buenos Aires, Argentina",
+  "node_addr": "api.testnet.lacchain.eosargentina.io",
+  "port_http": "443",
+  "port_ssl": "443",
+  "p2p_addr": "p2p.testnet.lacchain.eosargentina.io",
+  "port_p2p": "9870"
+},
+{
+ "bp_name": "ar3",
+ "organisation": "EOS Argentina",
+ "location": "Buenos Aires, Argentina",
+ "node_addr": "api.testnet.lacchain.eosargentina.io",
+ "port_http": "443",
+ "port_ssl": "443",
+ "p2p_addr": "p2p.testnet.lacchain.eosargentina.io",
+ "port_p2p": "9870"
+},
+{
+  "bp_name": "ar4",
+  "organisation": "EOS Argentina",
+  "location": "Buenos Aires, Argentina",
+  "node_addr": "api.testnet.lacchain.eosargentina.io",
+  "port_http": "443",
+  "port_ssl": "443",
+  "p2p_addr": "p2p.testnet.lacchain.eosargentina.io",
+  "port_p2p": "9870"
+},
+{
+ "bp_name": "ar5",
+ "organisation": "EOS Argentina",
+ "location": "Buenos Aires, Argentina",
+ "node_addr": "api.testnet.lacchain.eosargentina.io",
+ "port_http": "443",
+ "port_ssl": "443",
+ "p2p_addr": "p2p.testnet.lacchain.eosargentina.io",
+ "port_p2p": "9870"
+},
+{
+  "bp_name": "arsix",
+  "organisation": "EOS Argentina",
+  "location": "Buenos Aires, Argentina",
+  "node_addr": "api.testnet.lacchain.eosargentina.io",
+  "port_http": "443",
+  "port_ssl": "443",
+  "p2p_addr": "p2p.testnet.lacchain.eosargentina.io",
+  "port_p2p": "9870"
+},
+{
+  "bp_name": "cr1",
+  "organisation": "EOS Costa Rica",
+  "location": "San Jose, Costa Rica",
+  "node_addr": "latamlink.eosio.cr",
+  "port_http": "443",
+  "port_ssl": "443",
+  "p2p_addr": "latamlink.eosio.cr",
+  "port_p2p": "9876"
+},
+{
+  "bp_name": "cr2",
+  "organisation": "EOS Costa Rica",
+  "location": "San Jose, Costa Rica",
+  "node_addr": "latamlink.eosio.cr",
+  "port_http": "443",
+  "port_ssl": "443",
+  "p2p_addr": "latamlink.eosio.cr",
+  "port_p2p": "9876"
+},
+{
+  "bp_name": "cr3",
+  "organisation": "EOS Costa Rica",
+  "location": "San Jose, Costa Rica",
+  "node_addr": "latamlink.eosio.cr",
+  "port_http": "443",
+  "port_ssl": "443",
+  "p2p_addr": "latamlink.eosio.cr",
+  "port_p2p": "9876"
+},
+{
+  "bp_name": "cr4",
+  "organisation": "EOS Costa Rica",
+  "location": "San Jose, Costa Rica",
+  "node_addr": "latamlink.eosio.cr",
+  "port_http": "443",
+  "port_ssl": "443",
+  "p2p_addr": "latamlink.eosio.cr",
+  "port_p2p": "9876"
+},
+{
+  "bp_name": "cr5",
+  "organisation": "EOS Costa Rica",
+  "location": "San Jose, Costa Rica",
+  "node_addr": "latamlink.eosio.cr",
+  "port_http": "443",
+  "port_ssl": "443",
+  "p2p_addr": "latamlink.eosio.cr",
+  "port_p2p": "9876"
+},
+{
+  "bp_name": "crsix",
+  "organisation": "EOS Costa Rica",
+  "location": "San Jose, Costa Rica",
+  "node_addr": "latamlink.eosio.cr",
+  "port_http": "443",
+  "port_ssl": "443",
+  "p2p_addr": "latamlink.eosio.cr",
+  "port_p2p": "9876"
+},
+{
+  "bp_name": "ve1",
+  "organisation": "EOS Venezuela",
+  "location": "Caracas, Venezuela",
+  "node_addr": "lacchain.eosvenezuela.io",
+  "port_http": "443",
+  "port_ssl": "443",
+  "p2p_addr": "lacchain.eosvenezuela.io",
+  "port_p2p": "9876"
+},
+{
+  "bp_name": "ve2",
+  "organisation": "EOS Venezuela",
+  "location": "Caracas, Venezuela",
+  "node_addr": "lacchain.eosvenezuela.io",
+  "port_http": "443",
+  "port_ssl": "443",
+  "p2p_addr": "lacchain.eosvenezuela.io",
+  "port_p2p": "9876"
+},
+{
+  "bp_name": "ve3",
+  "organisation": "EOS Venezuela",
+  "location": "Caracas, Venezuela",
+  "node_addr": "lacchain.eosvenezuela.io",
+  "port_http": "443",
+  "port_ssl": "443",
+  "p2p_addr": "lacchain.eosvenezuela.io",
+  "port_p2p": "9876"
+},
+{
+  "bp_name": "ve4",
+  "organisation": "EOS Venezuela",
+  "location": "Caracas, Venezuela",
+  "node_addr": "lacchain.eosvenezuela.io",
+  "port_http": "443",
+  "port_ssl": "443",
+  "p2p_addr": "lacchain.eosvenezuela.io",
+  "port_p2p": "9876"
+},
+{
+  "bp_name": "ve5",
+  "organisation": "EOS Venezuela",
+  "location": "Caracas, Venezuela",
+  "node_addr": "lacchain.eosvenezuela.io",
+  "port_http": "443",
+  "port_ssl": "443",
+  "p2p_addr": "lacchain.eosvenezuela.io",
+  "port_p2p": "9876"
+},
+{
+  "bp_name": "vesix",
+  "organisation": "EOS Venezuela",
+  "location": "Caracas, Venezuela",
+  "node_addr": "lacchain.eosvenezuela.io",
+  "port_http": "443",
+  "port_ssl": "443",
+  "p2p_addr": "lacchain.eosvenezuela.io",
+  "port_p2p": "9876"
+}
 ]
